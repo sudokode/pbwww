62767e82e2edb6a9c0cbb529ec1979d7408165e5
sudokode <sudokode@gmail.com>
2016-01-22 23:35:45 -0500

    Add index.css

--- deploy.py
+++ deploy.py
@@ -4,20 +4,25 @@ import sys
 
 from lxml import html
 
-def main(index_html, index_js):
+def main(index_html, index_js, index_css):
     with open(index_html) as f:
         tree = html.parse(f)
 
-    script, = tree.xpath("//script[@id='index']")
+    script, = tree.xpath("//script[@id='index-script']")
 
     with open(index_js) as f:
         script.text = f.read()
         del script.attrib['src']
 
+    style, = tree.xpath("//style[@id='index-style']")
+
+    with open(index_css) as f:
+        style.text = f.read()
+
     sys.stdout.write(html.tostring(tree).decode('utf-8'))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("{} [index.html] [index.js]".format(sys.argv[0]))
+    if len(sys.argv) != 4:
+        sys.exit("{} [index.html] [index.js] [index.css]".format(sys.argv[0]))
 
     main(*sys.argv[1:])
